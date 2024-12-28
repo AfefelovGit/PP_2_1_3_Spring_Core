557a8ca288f64baccc7d98d8623c112a00629e45
Aleksei Fefelov <avvfefelov@yahoo.com>
2024-12-28 17:53:54 +0400

    PP_2_1_3_Spring_Core

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld beanHello1 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.printf("beanHello1.\"%s\"\n", beanHello1.getMessage());
+        HelloWorld beanHello2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.printf("beanHello2.\"%s\"\n", beanHello2.getMessage());
+        System.out.printf("(beanHello1 == beanHello2) = %s\n\n", (beanHello1 == beanHello2));
+
+        Cat catOne = applicationContext.getBean("cat", Cat.class);
+        System.out.printf("catOne.\"%s\"\n", catOne.getName());
+        Cat catTwo = applicationContext.getBean("cat", Cat.class);
+        System.out.printf("catTwo.\"%s\"\n", catTwo.getName());
+        System.out.printf("(catOne == catTwo) = %s\n", (catOne == catTwo));
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String name = null;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
